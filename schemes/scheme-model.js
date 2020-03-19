eb2f46394577760ede973920d9ef4e5c214175c5
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-19 13:22:08 -0500

    scheme model done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,8 +4,9 @@ module.exports = {
     find,
     findById,
     findSteps,
-    
-
+    add,
+    update,
+    remove
 }
 
 function find(){
@@ -17,5 +18,23 @@ function findById(id) {
 }
 
 function findSteps(id) {
-    return db('schemes').join('steps', 'steps.scheme_id', '=', 'schemes.id').where('schemes.id', id)
+    return db('schemes').join('steps', 'steps.scheme_id', '=', 'schemes.id').where('schemes.id', id);
 }
+
+function add(req){
+    return db('schemes').insert(req).then(scheme => {
+        return scheme;
+    })
+}
+
+function update(req, id){
+    return db('schemes').where('id', id).update(req);
+}
+
+function remove(id) {
+    return db("schemes").where('id', id).del()
+    .then(item => {
+        return item
+    });
+}
+
